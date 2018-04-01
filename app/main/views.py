bbeaf280662cf94db11fb9cf8c5eea0b9b24d435
snezhana <muginova2014@gmail.com>
2018-04-02 00:15:06 +0500

    Вычисление разницы цен между заданными датами (get параметры).

--- app/main/views.py
+++ app/main/views.py
@@ -6,6 +6,10 @@ from app.main.models import Price
 from app.main.models import InsiderTrade
 from app.main.models import Insider
 from sqlalchemy import func
+import datetime
+from flask import request
+from flask import redirect
+from flask import url_for
 
 
 # главная
@@ -52,3 +56,37 @@ def one_insider_view(ticker, insider_name):
                            insiders=trades,
                            title="Данные торговли совладельцев компании %s" % ticker,
                            )
+
+
+@main.route('/<string:ticker>/analytics', methods=['GET', 'POST'])
+def analytics_view(ticker):
+    if request.method == 'GET':
+        difference = dict()
+        date_from_value = request.args.get('date_from', None)
+        date_to_value = request.args.get('date_to', None)
+        try:
+            date_to = datetime.datetime.strptime(date_to_value, '%m-%d-%Y')
+            date_from = datetime.datetime.strptime(date_from_value, '%m-%d-%Y')
+        except:
+            return render_template('analytics.html',
+                                   title="Указаны неверные данные",
+                                   )
+        ticker = Ticker.query.filter(Ticker.name == ticker).first()  # находим нужную акцию
+        price_from = Price.query.filter(Price.ticker_id == ticker.id).filter(
+            Price.date == date_from).first()
+        price_to = Price.query.filter(Price.ticker_id == ticker.id).filter(
+            Price.date == date_to).first()
+
+        if price_from and price_to:
+            difference['open_price'] = round(price_from.open_price - price_to.open_price, 3)
+            difference['high'] = round(price_from.high - price_to.high, 3)
+            difference['low'] = round(price_from.low - price_to.low, 3)
+            difference['close_last'] = round(price_from.close_last - price_to.close_last, 3)
+            return render_template('analytics.html',
+                                   difference=difference,
+                                   title="Разница цен %s с %s по %s" % (ticker, date_from_value, date_to_value),
+                                   )
+        return render_template('analytics.html',
+                               title="Данные отсутствуют",
+                               )
+    return redirect(url_for('.ticker_view', ticker=ticker))
