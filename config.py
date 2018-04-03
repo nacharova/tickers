e475680c4550753877af95529b1c97e566323dd5
snezhana <muginova2014@gmail.com>
2018-04-04 01:45:41 +0500

    Добавлен readme. Изменен requirements.txt, убран ненужный код.

--- config.py
+++ config.py
@@ -1,40 +1,9 @@
 # -*- coding: utf-8 -*-
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 #  основные настройки
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY') or 'Bah6yee8Mievee6sAhVoh6AiUogh3eiheeGhi2au'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    FLASKY_MAIL_SUBJECT_PREFIX = os.getenv('FLASKY_MAIL_SUBJECT_PREFIX') or "none"
-    FLASKY_MAIL_SENDER = os.environ.get('FLASKY_MAIL_SENDER')
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_TO = os.environ.get('MAIL_TO')
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = '25'
-    MAIL_USE_SSL = False
-    MAIL_USE_TLS = False
-    BABEL_DEFAULT_LOCALE = 'ru'
-    BABEL_DEFAULT_TIMEZONE = 'Asia/Yekaterinburg'
-    YEAR_OF_BIRTH = 2017
-    ITEMS_PER_PAGE = 8
-    PREFIX_NAME = os.getenv('PREFIX_NAME') or 'tickers_'
-    CSRF_ENABLED = True
-
-    DEFAULT_FOLDER = os.path.join(os.path.dirname(__file__), 'default')
-    UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), 'app/static/data/')
-    UPLOAD_FOLDER_IMAGE = os.path.join(os.path.dirname(__file__), 'app/static/uploads/')
-    FONTS_FOLDER = os.path.join(os.path.dirname(__file__), 'app/static/fonts/')
-    THUMBNAIL_FOLDER = os.path.join(os.path.dirname(__file__), 'app/static/data/thumbnail/')
-    MAX_CONTENT_LENGTH = 10 * 1024 * 1024
-
-    try:
-        os.mkdir(UPLOAD_FOLDER)
-        os.mkdir(UPLOAD_FOLDER_IMAGE)
-    except OSError:
-        pass
 
     @staticmethod
     def init_app(app):
@@ -68,7 +37,7 @@ class ProductionConfig(Config):
         SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://tickers:tickers@192.168.0.71/postgres'
 
 
-#  варианты настроек, используються в __init__.py при вызове create_app из manage.py
+#  варианты настроек, используются в __init__.py при вызове create_app из manage.py
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
--- manage.py
+++ manage.py
@@ -5,17 +5,13 @@ import os
 from app import create_app
 from app import db
 from flask_script import Manager
-from flask_script import Shell
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-
 manager = Manager(app)
 
 
 def make_shell_context():
     return dict(app=app)
-
-manager.add_command("shell", Shell(make_context=make_shell_context))
 
 
 with app.app_context():
